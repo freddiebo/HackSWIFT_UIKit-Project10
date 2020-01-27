2391e608ed19010a60fa62998d4c84ab86047a6d
VBondarev <freddiebo@yandex.ru>
2020-01-27 10:40:10 +0300

    Initial Commit

---
+++ Project10/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Project10
+//
+//  Created by  Vladislav Bondarev on 27.01.2020.
+//  Copyright © 2020 Vladislav Bondarev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
